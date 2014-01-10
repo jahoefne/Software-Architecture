2f7d4f543cf4a6563f31b63d29755f17db2fb5c4
jahoefne <jahoefne@htwg-konstanz.de>
2014-01-10 16:07:38 +0100

    javadoc for interfaces

--- src/model/IField.java
+++ src/model/IField.java
@@ -6,18 +6,50 @@ package model;
  * User: jahoefne
  * Date: 10/01/14
  * Time: 12:20
+ * The Field represents the current Field state and may be altered using the methods declared in
+ * this interface
  */
 public interface IField {
 
+    /**
+     * Returns value of the cell at Point(x,y)
+     * @param x the x coordinate of the cell
+     * @param y  the x coordinate of the cell
+     * @return the value of the cell at Point(x,y)
+     **/
      byte getCell(int x, int y);
 
+    /**
+     * Returns value of the cell at Point p
+     * @param p the Point
+     * @return the value of the cell at Point p
+     **/
      byte getCell(Point p);
 
+    /**
+     * Moves pos1, to pos2. The value at pos2 is replaces with the value of pos1
+     * and pos1 set to 0
+     * @param pos1 the source position
+     * @param pos2 the destination position
+     **/
      void move(Point pos1, Point pos2);
 
+    /**
+     * Sets the cell at point p to the value value
+     * @param p the position cell to alter
+     * @param value the new value for the cell at p
+     **/
      void set(Point p, byte value);
 
+    /**
+     * Toggles whether it is the white or the black players turn
+     **/
      void toggleWhiteOrBlack();
 
+
+    /**
+     * Returns whether it's the white or the black players turn
+     * @return <0 if black turn, >0 of whites turn
+     */
      byte getWhiteOrBlack();
 }
--- src/model/IFigureMask.java
+++ src/model/IFigureMask.java
@@ -4,10 +4,31 @@ package model;
  * User: jahoefne
  * Date: 10/01/14
  * Time: 12:29
+ * Describes a figure, all figures are defined in the Figures enum
  */
 public interface IFigureMask {
-     byte getId();
-     int getBitMask();
-     boolean isLimited();
-     String getUnicode();
+    /**
+     * Gets the internal representation of the figure
+     * @return the internal representation
+     **/
+    byte getId();
+
+    /**
+     * Returns the bitmask which describes the figures move and capture capabilities
+     * @return the figure capabilities
+     **/
+    int getBitMask();
+
+    /**
+     * Returns wheter the figures movement is limite to one field of unlimited
+     * E.g. Queens movement is unlimites, Kings movement limited
+     * @return true if movement is limites, false if movement is unlimited
+     **/
+    boolean isLimited();
+
+    /**
+     * Returns the unicode representation of the figure
+     * @return the unicode sign of the figure
+     **/
+    String getUnicode();
 }
