fddea8e124f5062abf79bcfe6c5da3e5937f8d87
jahoefne <jahoefne@htwg-konstanz.de>
2014-01-10 15:42:11 +0100

    javadoc for IGameController

--- src/controller/IGameController.java
+++ src/controller/IGameController.java
@@ -10,25 +10,57 @@ import java.awt.*;
  */
 public interface IGameController {
 
-    // restarts the game
+    /**
+     * Restarts the game
+     *
+     * <p>
+     * Restarts the game, by resetting, the model and controller state,
+     * the caller must assure to also reset the UI.
+     * <p>
+     */
     void resetGame();
 
-    // get an array of all possible moves of the figure at point p
+    /**
+     * Returns an Array of Points the Figure at Point p is able to move to
+     *
+     * @param p The source field
+     * @return The possible fields p may be moved to
+     */
     Point[] getPossibleMoves(Point p);
 
-    // moves figure at point x, to point y, returns if the move was successful
+    /**
+     * Tries to move the Figure at field x to field y
+     *
+     * @param x the source field
+     * @param y the destination field
+     * @return TRUE, if move successful. FALSE, if move not legal
+     */
     boolean move(Point x, Point y);
 
-    // returns the unicode representation of the figure at point x
+    /**
+     * Returns the unicode string representing the figure at field x
+     * @param x The field
+     */
     String getUnicode(Point x);
 
-    // is used to determine the colour of a figure on field x
-    // >0 white, <0 black
+
+    /**
+     * Returns the model representation of the field x
+     * @param x The field
+     * @return 0 if the field is empty. <0 if the field is occupied by a black figure,
+     * >0 if it is occupied by a white figure
+     */
     byte getID(Point x);
 
-    // obvious
+    /**
+     * Returns if it's the white players, or black players turn
+     * @return TRUE, if white players turn, FALSE if black players turn
+     */
     boolean whitesTurn();
 
-    // obvious
+    /**
+     * Checks if the field is in checkmate position
+     * @return TRUE, if checkmate, FALSE not checkmate
+     */
     boolean isCheckMate();
 }
