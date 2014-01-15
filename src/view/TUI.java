18c317d42cd449f596e70ad93fa32b9209699684
jahoefne <jahoefne@htwg-konstanz.de>
2014-01-15 23:46:38 +0100

    coding style, sonar

--- src/view/TUI.java
+++ src/view/TUI.java
@@ -10,109 +10,107 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 
-public final class TUI implements IObserver{
-	
-	private static String newLine = System.getProperty("line.separator");
-	private static final String enter_command = "Enter your move or HELP:";
-	private String message = null;
-	private static Logger logger = Logger.getLogger("TUI.class");
-    
-    private IGameController GAME_CONTROLLER;
+public final class TUI implements IObserver {
+
+    private static String newLine = System.getProperty("line.separator");
+    private static final String ENTER_COMMAND = "Enter your move or HELP:";
+    private String message = null;
+    private static Logger logger = Logger.getLogger("TUI.class");
+
+    private static IGameController gameController;
 
 
     public TUI(IGameController controller) {
-    	this.GAME_CONTROLLER = controller;
-    	controller.addObserver(this);
+        this.gameController = controller;
+        controller.addObserver(this);
     }
 
-    public void printTUI(){
-    	logger.info(GAME_CONTROLLER);
-    	if(message != null){
-    		logger.info(message+newLine);
-    	}
-    	message = null;
-    	logger.info(enter_command+newLine);
-    }
-    
-    public boolean read_input(){
-		 String input = null;
-	    
-	     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	     
-	     try {
-			input = br.readLine();
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	    return handle_input(input);
-    }
-    
-    private void display_help(){
-    	String helptext = "----valid commands----\n" +
-    			"NEW GAME - Starts a new game of chess\n"+
-    			"MOVE A B - Moves a piece from point A to point B\n"+
-    			"           Example: MOVE A2 A4\n"+
-    			"EXIT     - Close the game\n"+
-    			"HELP     - Display help\n";
-    	logger.info(helptext);
-    	logger.info("Enter your move or HELP:"+newLine);
-    	read_input();
-
-    }
-    private boolean handle_input(String input){
-    	
-    	String[] input_splitted = input.split(" ");
-
-    	if(input.equals("HELP") && input_splitted.length == 1){
-        	display_help();
-        }else if(input_splitted[0].equalsIgnoreCase("NEW")
-    			&& input_splitted[1].equalsIgnoreCase("GAME")
-    			&& input_splitted.length == 2){
-    		GAME_CONTROLLER.resetGame();
-    		
-    	}else if(input_splitted[0].equalsIgnoreCase("MOVE") &&
-    			valid_point(input_splitted[1])&&
-    			valid_point(input_splitted[2])&&
-    			input_splitted.length == 3){
-        		execute_move(input_splitted[1], input_splitted[2]);
-        		
-    	}else if(input.equals("EXIT")){
-    		logger.info("Exiting Game");
-    		return false;
-    	}else{
-    		logger.info("Invalid command, type HELP for help");
-    	}
-    	return true;
-    }
-    
-    private static boolean valid_point(String point){
-    	int a = (int) point.charAt(0);
-    	int b = point.charAt(1) - '0';
-    	if(a >= (int)'A' && a <= (int)'H'
-			&& b >= 1 && b <= 8
-			&& point.length() == 2){
-    		return true;   		
-    	}else{
-    		return false;
-    	}
-    }
-    private void execute_move(String from, String to){
-    	int from_x = from.charAt(0) - 65;
-		int from_y = 8 - (from.charAt(1) - '0');		
-		int to_x = to.charAt(0) - 65;
-		int to_y = 8 - (to.charAt(1) - '0');
-		if(GAME_CONTROLLER.move(new Point(from_x,from_y), new Point(to_x, to_y))){
-			logger.info("Last move was valid!");
-		}else{
-			logger.info("Last move was invalid!");
-		}
-		
+    public void printTUI() {
+        logger.info(gameController);
+        if (message != null) {
+            logger.info(message + newLine);
+        }
+        message = null;
+        logger.info(ENTER_COMMAND + newLine);
     }
 
-	@Override
-	public void update(Event e) {
-		
+    public boolean readInput() {
+        String input = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            input = br.readLine();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return handleInput(input);
+    }
+
+    private void displayHelp() {
+        String helpText = "----valid commands----\n" +
+                "NEW GAME - Starts a new game of Chess\n" +
+                "MOVE A B - Moves a piece from point A to point B\n" +
+                "           Example: MOVE A2 A4\n" +
+                "EXIT     - Close the game\n" +
+                "HELP     - Display help\n";
+        logger.info(helpText);
+        logger.info("Enter your move or HELP:" + newLine);
+        readInput();
+
+    }
+
+    private boolean handleInput(String input) {
+        String[] inputSplitted = input.split(" ");
+
+        if (input.equals("HELP") && inputSplitted.length == 1) {
+            displayHelp();
+        } else if (inputSplitted[0].equalsIgnoreCase("NEW")
+                && inputSplitted[1].equalsIgnoreCase("GAME")
+                && inputSplitted.length == 2) {
+            gameController.resetGame();
+
+        } else if (inputSplitted[0].equalsIgnoreCase("MOVE") &&
+                validPoint(inputSplitted[1]) &&
+                validPoint(inputSplitted[2]) &&
+                inputSplitted.length == 3) {
+            executeMove(inputSplitted[1], inputSplitted[2]);
+
+        } else if (input.equals("EXIT")) {
+            logger.info("Exiting Game");
+            return false;
+        }
+
+        logger.info("Invalid command, type HELP for help");
+        return true;
+    }
+
+    private static boolean validPoint(String point) {
+        int a = (int) point.charAt(0);
+        int b = point.charAt(1) - '0';
+        if (a >= (int) 'A' && a <= (int) 'H'
+                && b >= 1 && b <= 8
+                && point.length() == 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void executeMove(String from, String to) {
+        int fromX = from.charAt(0) - 65;
+        int fromY = 8 - (from.charAt(1) - '0');
+        int toX = to.charAt(0) - 65;
+        int toY = 8 - (to.charAt(1) - '0');
+        if (gameController.move(new Point(fromX, fromY), new Point(toX, toY))) {
+            logger.info("Last move was valid!");
+        } else {
+            logger.info("Last move was invalid!");
+        }
+    }
+
+    @Override
+    public void update(Event e) {
         System.out.println("notified!");
-		printTUI();
-	}
+        printTUI();
+    }
 }
