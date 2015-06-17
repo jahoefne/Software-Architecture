870a89df173c2435832803919281680577959c18
jahoefne <jahoefne@htwg-konstanz.de>
2015-06-17 19:35:06 +0200

    implement Method Stubs

--- src/persistence/CouchGameDB.java
+++ src/persistence/CouchGameDB.java
@@ -1,7 +1,35 @@
 package persistence;
 
+import controller.GameController;
+
+import java.util.List;
+
 /**
  * Created by moe on 17/06/15.
  */
-public class CouchGameDB {
+public class CouchGameDB implements IGameDB {
+    @Override
+    public boolean doeGameExistWithUUID(String uuid) {
+        return false;
+    }
+
+    @Override
+    public void saveGame(GameController game) {
+
+    }
+
+    @Override
+    public GameController loadGameWithUUID(String uuid) {
+        return null;
+    }
+
+    @Override
+    public void deleteGameWithUUID(String uuid) {
+
+    }
+
+    @Override
+    public List<GameController> listGames(String uuid) {
+        return null;
+    }
 }
