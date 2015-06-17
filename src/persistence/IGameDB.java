28bbaa24ddc461d73e8c361527cd52a35f4f5413
jahoefne <jahoefne@htwg-konstanz.de>
2015-06-17 19:34:04 +0200

    define Methods for Database Access

--- src/persistence/IGameDB.java
+++ src/persistence/IGameDB.java
@@ -1,7 +1,22 @@
 package persistence;
 
-/**
- * Created by moe on 17/06/15.
- */
+import controller.GameController;
+
+import java.util.List;
+
 public interface IGameDB {
+    /** returns true if a game with uuid exists */
+    boolean doeGameExistWithUUID(String uuid);
+
+    /** Saves an GameController in the Database */
+    void saveGame(GameController game);
+
+    /** load the game with the given uuid from the database **/
+    GameController loadGameWithUUID(String uuid);
+
+    /** Delete the game with uuid from the database */
+    void deleteGameWithUUID(String uuid);
+
+    /** Return a list of all games created by uuid */
+    List<GameController> listGames(String uuid);
 }
