c1acd249cc739421813db547cc44cb2526c849cd
Tommy <TommyBruenske@online.de>
2015-07-01 07:53:47 +0200

    NoPawnsTest coverage

--- test/plugin/NoPawnsTest.java
+++ test/plugin/NoPawnsTest.java
@@ -1,32 +1,39 @@
 package plugin;
 
+import controller.GameController;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.awt.*;
 
 import static org.junit.Assert.*;
 
 public class NoPawnsTest {
     private NoPawns clazz;
+    GameController gc;
 
     @Before
     public void setUp() throws Exception {
         clazz = new NoPawns();
+        gc = new GameController("0"," a");
     }
 
     @After
     public void tearDown() throws Exception {
-
+        
     }
 
     @Test
     public void testGameCreatedPlugin() throws Exception {
-
+        clazz.gameCreatedPlugin(gc);
+        assertFalse(clazz.getGameOver());
     }
 
     @Test
     public void testMoveCalledPlugin() throws Exception {
-
+        clazz.moveCalledPlugin(gc, new Point(0, 0), new Point(1, 0));
+        assertFalse(clazz.getGameOver());
     }
 
     @Test
